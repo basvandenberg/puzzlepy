d67bcfcb4d26adab3498f664f3d89e6393bb5e06
basvandenberg <b.a.vandenberg@gmail.com>
2015-08-19 21:29:57 +0200

    Setup 0.1.0.

--- setup.py
+++ setup.py
@@ -4,15 +4,24 @@ config = {
 
     'name': 'puzzlepy',
     'version': 0.1.0,
-    'description': '',
     'author': 'B.A. van den Berg',
     'authon_email': 'b.a.vandenberg@gmail.com',
-    'url': '',
+    'url': 'https://github.com/basvandenberg/puzzlepy',
+    'description': 'Python package for generating, solving, and testing grid-based puzzle games.',
+    'download_url': 'https://github.com/basvandenberg/puzzlepy/releases',
     'license': 'MIT',
-    'classifiers': [],
-    'keywords': [],
-    'install_requires': ['nose']
     'packages': ['puzzlepy'],
+    'classifiers': [
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Games/Entertainment :: Puzzle Games',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 }
 
 setup(**config)
