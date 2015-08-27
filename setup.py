df8074f74c467c933bc26be40d3d58c9d7c65a89
basvandenberg <b.a.vandenberg@gmail.com>
2015-08-27 19:42:08 +0200

    Bumped version number to 0.1.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 config = {
 
     'name': 'puzzlepy',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'author': 'B.A. van den Berg',
     'author_email': 'b.a.vandenberg@gmail.com',
     'url': 'https://github.com/basvandenberg/puzzlepy',
